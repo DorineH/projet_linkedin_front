72cdaa135b14ba51933fa1e6ac101cdb2db008e4
DorineH <dorinematildehenry@gmail.com>
2025-12-10 17:58:47 +0100

    fix front deploiement vercel 2

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,18 +1,18 @@
-// import axios from 'axios'
+import axios from "axios";
 
+// Une seule variable pour toutes les requêtes
+const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_BASE || 'http://127.0.0.1:8000',
-//     timeout: 15000,
-// })
+// Axios configuré avec l'URL backend
+const api = axios.create({
+    baseURL: API_URL,
+    timeout: 15000,
+});
 
-
-// export default api
-
-// api.js
-const API_URL = import.meta.env.VITE_API_URL;
-
+// Exemple d’appel API en fetch
 export async function getJobs() {
-    const res = await fetch(`${API_URL}/`);
+    const res = await fetch(`${API_URL}`);
     return res.json();
 }
+
+export default api;
